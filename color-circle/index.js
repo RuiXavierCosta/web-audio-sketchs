9da60c5ca00241d33fc01cb7c32bc75e5f07364f
RuiXavierCosta <ruixaviercosta@gmail.com>
2019-06-06 22:04:41 +0100

    init

--- color-circle/index.js
+++ color-circle/index.js
@@ -13,7 +13,7 @@ window.requestAnimFrame = (function() {
     }
   );
 })();
-
+const canvas = document.querySelector("canvas");
 let audioPlaying = false;
 
 const context = new AudioContext();
@@ -63,7 +63,7 @@ function startPlaying(audioData) {
   javascriptNode.onaudioprocess = function() {
     // get the Frequency Domain data for this sample
     analyserNode.getByteFrequencyData(frequencyArray);
-
+    frequencyArray.slice(0, SAMPLE_SIZE * (2 / 3));
     // draw the display if the audio is playing
     if (audioPlaying == true) {
       requestAnimFrame(drawFrequencyDomain);
@@ -76,31 +76,77 @@ function startPlaying(audioData) {
 }
 
 function drawFrequencyDomain() {
-  clearFrequencyCanvas();
-  frequencyCanvas.lineWidth = 2;
-  const pastValues = { x: -1, y: CANVAS_HEIGHT };
+  const sum = frequencyArray.reduce(function(a, b) {
+    return a + b;
+  });
+  const avg = sum / frequencyArray.length;
 
-  for (var i = 0; i < frequencyArray.length; i++) {
+  clearFrequencyCanvas(avg / CANVAS_HEIGHT);
+  frequencyCanvas.lineWidth = 2;
+  const radius = 50;
+  const center = { x: CANVAS_WIDTH / 2, y: CANVAS_HEIGHT / 2 };
+  const pastValues = {
+    x:
+      center.x -
+      (2 * radius) / 10 -
+      (8 * ((2 * Math.PI * frequencyArray[0]) / CANVAS_HEIGHT) * radius) / 10,
+    y: center.y,
+    invertedX:
+      center.x -
+      (2 * radius) / 10 -
+      (8 * ((2 * Math.PI * frequencyArray[0]) / CANVAS_HEIGHT) * radius) / 10,
+    invertedY: center.y
+  };
+
+  for (var i = 1; i < (2 * frequencyArray.length) / 3; i++) {
     const value = frequencyArray[i] / CANVAS_HEIGHT;
-    const x = i / (frequencyArray.length / CANVAS_WIDTH) - 1;
-    const y = CANVAS_HEIGHT - CANVAS_HEIGHT * value;
+    // const x = i / (frequencyArray.length / CANVAS_WIDTH) - 1;
+    // const y = CANVAS_HEIGHT - CANVAS_HEIGHT * value;
+
+    const alpha = (i / ((2 * frequencyArray.length) / 3)) * 2 * Math.PI;
+    const beta = value * 2 * Math.PI;
+    const adaptedRadius = (2 * radius) / 10 + (8 * beta * radius) / 10;
+
+    const x = center.x - Math.cos(alpha / 2) * adaptedRadius;
+    const y = center.y + Math.sin(alpha / 2) * adaptedRadius;
     // const hue = 360 * (x / frequencyArray.length);
     const hue = 360 * value;
-    const saturation = 50 + Math.ceil(value * 100) / 2;
+    const saturation = Math.ceil(value * 100) / 2;
     // const light = 25 + (3 * Math.ceil(value * 100)) / 4;
-    const light = 25 + 75 * (x / frequencyArray.length);
+    const light = 100 * (x / frequencyArray.length);
 
     frequencyCanvas.beginPath();
+    // frequencyCanvas.arc(x, y, 4, 0, 2 * Math.PI, true); // Inner: CW
+
     frequencyCanvas.moveTo(pastValues.x, pastValues.y);
-    frequencyCanvas.strokeStyle = `hsl(${hue},${saturation}%,${light}%)`;
     frequencyCanvas.lineTo(x, y);
     frequencyCanvas.stroke();
+    frequencyCanvas.beginPath();
+    // frequencyCanvas.arc(x, y, 4, 0, 2 * Math.PI, true); // Inner: CW
+
+    const invertedX = center.x - Math.cos(-alpha / 2) * adaptedRadius;
+    const invertedY = center.y + Math.sin(-alpha / 2) * adaptedRadius;
+    frequencyCanvas.moveTo(pastValues.invertedX, pastValues.invertedY);
+    frequencyCanvas.lineTo(invertedX, invertedY);
+    frequencyCanvas.stroke();
+    frequencyCanvas.strokeStyle = `hsl(${hue},${saturation}%,${light}%)`;
+    // frequencyCanvas.closePath();
+
+    // frequencyCanvas.fillStyle = `hsl(${hue},${saturation}%,${light}%)`;
+    // frequencyCanvas.fill();
+
+    frequencyCanvas.lineWidth = adaptedRadius;
 
     pastValues.x = x;
     pastValues.y = y;
+    pastValues.invertedX = invertedX;
+    pastValues.invertedY = invertedY;
   }
 }
 
-function clearFrequencyCanvas() {
+function clearFrequencyCanvas(maxValue) {
+  const hue = 360 * maxValue;
+  const saturation = Math.ceil(maxValue * 100) / 2;
+  canvas.style.backgroundColor = `hsl(${hue},${saturation}%,50%)`;
   frequencyCanvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
